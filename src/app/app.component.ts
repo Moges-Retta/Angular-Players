8a9daae48ec83d28ab24883b10ff4ba4d2788a2d
Moges <Moges.ash@gmail.com>
2020-10-07 15:50:20 +0200

    apart html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,44 +2,8 @@ import { Component } from '@angular/core';
 import { Land } from 'src/model/land';
 @Component({
   selector: 'app-root',
-  template: `<h1>{{title}}</h1>
-  <h2>Details van {{land.name}}</h2>
-  <div><label>id: </label>{{land.id}}</div>
-  <div><label>naam: </label>{{land.name}}
-  <input [(ngModel)]="land.name" placeholder="name"></div>
-  <h1>{{title}}</h1>
-            <h2>Mijn landen</h2>
-            <ul class="landen">
-              <li *ngFor="let l of landen" (click)="onSelect(l)" [class.selected]="l===selectedLand">>
-                <span>{{l.id}}</span>{{l.name}}
-              </li>
-            </ul>
-  <div *ngIf="selectedLand">
-   <h2>{{selectedLand.name}} details!</h2>
-   <div>
-<label>id: </label>{{selectedLand.id}}
-   </div>
-   <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedLand.name" placeholder="name"
-      [style.background-color]="selectedLand.name===''? 'red' :'white'"/>
-   </div>
-</div>`,
-styles: [`
-  .landen li{
-    cursor:pointer;
-  }
-  .selected{
-    background-color:#CFD8DC;
-    color:white;
-  }
-.landen li:hover{
-    background-color: #BBD8DC;
-    color:white
-  }
-`],
-
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
 
